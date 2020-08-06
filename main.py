95c79d1b325164a3eb5e9fcb9ef5d034f855f018
jesseHardee <jwrhardee@gmail.com>
2020-08-06 20:47:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+#functions insturtion to do something
+
+#define em
+#call them
+#   print ("hello")
+
+# greeting()
+
+# def goodbye(name):
+#   print("goodbye"+name)
+
+# goodbye(" mario")
+
+# def add(num):
+#   for i in range(5):
+#     print(sum)
+#     if sum < 5:
+#       print("less than 5")
+#      else:
+#        print ("greater than 5")
+
+# add (3)
+
+#def sandwich(meat):
+#   print("I like " + meat + "sandwichs")
+# def add(num):
+#   for i in range(5)
+
+# sandwich("turkey")
+
+# def sum (n1, n2):
+#   return n1 + n2 
+#   total= sum  (1,3)
+
+
+
+
+
+#total = sum(2, 5)
+# def sum2 (n1,n2):
+#   print(n1 + n2)
+# sum2 (4234234,234234)
+
+
+
+
+def book(Book):
+  print (Book)
+
+book("lord of the rings")
